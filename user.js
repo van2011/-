f7df40c4ce0ca7e715c80f129bf241615898f21c
van2011 <van2011111111111@gmail.com>
2023-12-10 19:18:12 +0200

    da

--- user.js
+++ user.js
@@ -4,4 +4,31 @@ $(document).ready(function () {
         $(".userdescriptiontext").toggle(1000)
     })
 
+    $.ajax("https://my-json-server.typicode.com/van2011/test/products", {
+        dataType: "json",
+        data: { limit: 127 },
+        success: function (result) {
+            result.forEach(e => {
+                console.log(e);
+                $('.kvadratkvadrat').append(`
+
+    <div class="kvadrat">
+    
+            <div class="ititle itext">${e.name}</div>
+            <div class="ititle itext">${sir.name}</div>
+            <img class="ipng itext"
+                src="${e.photo_url}" />
+            
+            <div class="descriptiontext">${e.balance}</div>
+            
+      
+        </div>
+        
+        `)
+            })
+        }
+    })
+
 })
+
+
